200bd0d4e7563624133d819a5dd97858a6b10fc2
LucasOe <github@lucasoe.com>
2022-07-18 19:34:10 +0200

    Add animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,33 @@
-import { PerspectiveCamera } from "@react-three/drei";
-import { Canvas } from "@react-three/fiber";
-import React from "react";
+import { OrbitControls, PerspectiveCamera } from "@react-three/drei";
+import { Canvas, useFrame } from "@react-three/fiber";
+import React, { useRef } from "react";
 import "./App.scss";
+
+function Cube() {
+	const meshRef = useRef();
+
+	useFrame(() => {
+		meshRef.current.rotation.x += 0.01;
+		meshRef.current.rotation.y += 0.01;
+	});
+
+	return (
+		<mesh ref={meshRef}>
+			<boxGeometry />
+			<meshStandardMaterial />
+		</mesh>
+	);
+}
 
 export default function App() {
 	return (
 		<div className="App">
 			<Canvas>
-				<PerspectiveCamera makeDefault fov={75} position={[0, 0, 10]} />
-				<ambientLight />
-				<mesh>
-					<boxGeometry />
-					<meshStandardMaterial />
-				</mesh>
+				<PerspectiveCamera makeDefault fov={75} position={[0, 0, 5]} />
+				<OrbitControls enableZoom={false} />
+				<ambientLight intensity={0.1} />
+				<directionalLight position={[0, 0, 5]} intensity={1} />
+				<Cube />
 			</Canvas>
 		</div>
 	);
