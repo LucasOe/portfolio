a8bd2e1bb1fdb9d54b9562de5dbc765013ea7b86
LucasOe <lucas.oelker@gmx.de>
2022-11-04 20:54:19 +0100

    Set max_line_length to 120

--- src/App.tsx
+++ src/App.tsx
@@ -8,22 +8,16 @@ export default function App() {
 			<div className="main">
 				<h1 className="header">Hey!</h1>
 				<p className="text">
-					My name is Lucas Oelker and I&apos;m currently studying Visual
-					Computing and Design in Lippstadt, Germany. Programming, rendering or
-					web design — if it requires a computer and some creative thinking
-					I&apos;m interested!
+					My name is Lucas Oelker and I&apos;m currently studying Visual Computing and Design in Lippstadt, Germany.
+					Programming, rendering or web design — if it requires a computer and some creative thinking I&apos;m
+					interested!
 				</p>
 				<p className="text">
-					This is supposed to be a portfolio at some point in the future, but
-					right now it&apos;s still in development. Why don&apos;t you check out
-					my socials down below in the meantime?
+					This is supposed to be a portfolio at some point in the future, but right now it&apos;s still in development.
+					Why don&apos;t you check out my socials down below in the meantime?
 				</p>
 				<div className="socials">
-					<a
-						href="http://twitter.com/_LucasOe_"
-						target="_blank"
-						rel="noreferrer"
-					>
+					<a href="http://twitter.com/_LucasOe_" target="_blank" rel="noreferrer">
 						<img src={twitter} alt="Twitter" className="logo" />
 					</a>
 					<a href="http://github.com/LucasOe" target="_blank" rel="noreferrer">
