6ca40a496b9332fa33a29b02bcfd56a1f511211a
LucasOe <github@lucasoe.com>
2022-11-04 20:24:52 +0100

    Add vite-plugin-webfont-dl

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import webfontDownload from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		webfontDownload([
+			"https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap",
+		]),
+	],
 });
