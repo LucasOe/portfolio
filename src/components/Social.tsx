6ab33231245879ed250ec9373cbcad198d2c549f
LucasOe <github@lucasoe.com>
2022-11-05 01:07:51 +0100

    Add mailto

---
+++ src/components/Social.tsx
@@ -0,0 +1,19 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import "../styles/components/social.scss";
+
+type SocialProps = {
+	icon: IconDefinition;
+	link: string;
+	newtab: boolean;
+};
+
+export default function Social({ icon, link, newtab }: SocialProps) {
+	return (
+		<div className="iconWrapper">
+			<a href={link} target={newtab ? "_blank" : "_self"} rel="noreferrer">
+				<FontAwesomeIcon icon={icon} className="icon" />
+			</a>
+		</div>
+	);
+}
