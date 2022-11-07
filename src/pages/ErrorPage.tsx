6df5173da57699b71f1fab5ab34a94a763de2197
LucasOe <github@lucasoe.com>
2022-11-07 13:55:48 +0100

    Add prettier-plugin-tailwindcss

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -9,9 +9,9 @@ export default function ErrorPage() {
 	const error: ErrorResponse = useRouteError() as ErrorResponse;
 
 	return (
-		<div className="min-h-screen flex justify-center bg-neutral-900 p-6">
+		<div className="flex min-h-screen justify-center bg-neutral-900 p-6">
 			<div className="h-full sm:pt-20 md:pt-40 lg:pt-60 xl:pt-80">
-				<div className="prose prose-lg prose-invert max-w-none text-neutral-200">
+				<div className="prose-lg prose prose-invert max-w-none text-neutral-200">
 					<h1>{error.statusText}</h1>
 					<p>It looks like there was an error.</p>
 				</div>
