3397b21469bba0c5a015950802e9f680c0c98624
LucasOe <github@lucasoe.com>
2022-11-07 17:22:41 +0100

    Add aria-label for accessability

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -10,7 +10,7 @@ type SocialProps = {
 export default function Social({ icon, link, newtab }: SocialProps) {
 	return (
 		<div className="h-10 w-10 rounded-full bg-neutral-200 p-2 opacity-80 hover:opacity-100">
-			<a href={link} target={newtab ? "_blank" : "_self"} rel="noreferrer">
+			<a href={link} target={newtab ? "_blank" : "_self"} rel="noreferrer" aria-label={link}>
 				<FontAwesomeIcon icon={icon} className="h-full w-full text-neutral-900" />
 			</a>
 		</div>
