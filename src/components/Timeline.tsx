a8d25b1fa91a79e37cc2c7cbccaa3a15a111b28a
LucasOe <lucas.oelker@gmx.de>
2023-01-17 20:29:35 +0100

    Add comments

--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -15,21 +15,25 @@ export default function Timeline({ children, unixBegin, unixEnd, className }: Da
 	const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	const currentTime = Math.floor((unixEnd - unixBegin) * percentage) + unixBegin;
 	const date = new Date(currentTime * 1000);
+	const dateText = `${months[date.getMonth()]} ${date.getFullYear()}`;
 
 	return (
 		<div className={className}>
 			<div className="flex">
 				{children}
 				<div className="relative hidden sm:flex">
-					<div className="my-4 mx-2 w-2 rounded-full bg-neutral-700" />
+					{/*Progress Bar*/}
+					<div className="my-4 mx-2 w-2 rounded-full  bg-neutral-700" />
+					{/*Sticky*/}
 					<div className="relative -left-6 -mr-6">
 						<div ref={inputRef} className="sticky top-1/2 flex">
+							{/*Dot*/}
 							<div className="mx-1 my-2 h-4 w-4 rounded-full bg-emerald-400" />
+							{/*Arrow*/}
 							<div className="my-2 h-0 w-0 border-y-8 border-r-8 border-y-transparent border-r-neutral-700" />
+							{/*Textbox*/}
 							<div className="w-28 rounded-md bg-neutral-700 text-center">
-								<p className="m-0 font-mono font-bold">{`${
-									months[date.getMonth()]
-								} ${date.getFullYear()}`}</p>
+								<p className="m-0 font-mono font-bold">{dateText}</p>
 							</div>
 						</div>
 					</div>
