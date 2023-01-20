865504ad5603f72d741397308715dc8a578150e0
LucasOe <lucas.oelker@gmx.de>
2023-01-20 23:54:06 +0100

    Edit Projects Component

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,19 +1,17 @@
 type ProjectProps = {
+	children: React.ReactNode;
 	title: string;
+	link?: string;
 	arrowPosition: number;
 	className?: string;
 };
 
-export default function Project({ title, arrowPosition, className }: ProjectProps) {
+export default function Project({ children, title, arrowPosition, className }: ProjectProps) {
 	return (
 		<div className={`${className} flex transition-transform duration-300 hover:-translate-x-2`}>
-			<div className="rounded-md bg-neutral-700 p-4">
+			<div className="w-full rounded-md bg-neutral-700 p-4">
 				<h3 className="m-0 mb-2">{title}</h3>
-				<p className="m-0">
-					Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aspernatur tempore fuga ea, iste inventore
-					deserunt incidunt repudiandae, debitis veritatis neque non, aliquid laudantium distinctio labore
-					architecto voluptas odio cupiditate reprehenderit.
-				</p>
+				<p className="m-0">{children}</p>
 			</div>
 			{/*Arrow*/}
 			<div className="hidden h-full py-2 sm:flex">
