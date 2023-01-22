bb6829165965857f0a7a3f43df78f03eea5f7f27
LucasOe <github@lucasoe.com>
2023-01-23 00:02:57 +0100

    Add icons to projects

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,17 +1,32 @@
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
+import { faUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 type ProjectProps = {
 	children: React.ReactNode;
 	title: string;
-	link?: string;
+	link: string;
+	stack: IconDefinition[];
 	arrowPosition: number;
 	className?: string;
 };
 
-export default function Project({ children, title, arrowPosition, className }: ProjectProps) {
+export default function Project({ children, title, link, stack, arrowPosition, className }: ProjectProps) {
 	return (
 		<div className={`${className} flex transition-transform duration-300 hover:-translate-x-2`}>
-			<div className="w-full rounded-md bg-neutral-700 p-4">
-				<h3 className="m-0 mb-2">{title}</h3>
+			<div className="flex w-full flex-col space-y-2 rounded-md bg-neutral-700 p-4">
+				<div className="flex items-center justify-between">
+					<h3 className="m-0">{title}</h3>
+					<a href={link} target="_blank" rel="noreferrer">
+						<FontAwesomeIcon icon={faUpRightFromSquare} className="h-6 w-6 hover:text-green-500" />
+					</a>
+				</div>
 				<p className="m-0">{children}</p>
+				<div className="flex space-x-2">
+					{stack.map((icon) => (
+						<FontAwesomeIcon key={icon.iconName} icon={icon} className="h-7 w-7" />
+					))}
+				</div>
 			</div>
 			{/*Arrow*/}
 			<div className="hidden h-full py-2 sm:flex">
