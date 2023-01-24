b422f1966f9af6dce80a899af241c724d791ba44
LucasOe <lucas.oelker@gmx.de>
2023-01-24 01:23:20 +0100

    Add Tooltip

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -81,10 +81,22 @@ export default function Project({ children, title, link, stack, arrowPosition, c
 				<p className="m-0">{children}</p>
 				<div className="flex space-x-2">
 					{stack.map((icon) => (
-						<div key={icons[icon].name} className="h-7 w-7 rounded-md bg-neutral-200 p-1">
-							<IconContext.Provider value={{ className: "w-full h-full text-neutral-900" }}>
-								{icons[icon].icon}
-							</IconContext.Provider>
+						<div key={icons[icon].name}>
+							<div className="h-7 w-7 rounded-md bg-neutral-200 [&>#tooltip]:hover:flex">
+								<IconContext.Provider value={{ className: "w-full h-full text-neutral-900 p-1" }}>
+									{icons[icon].icon}
+								</IconContext.Provider>
+								<div id="tooltip" className="absolute mt-1 hidden flex-col">
+									{/*Arrow*/}
+									<div className="relative left-[6px] h-0 w-0 border-x-8 border-b-8 border-x-transparent border-b-neutral-200" />
+									{/*Textbox*/}
+									<div className="rounded-md bg-neutral-200 p-1 px-2">
+										<p className=" m-0 font-mono leading-none text-neutral-700">
+											{icons[icon].name}
+										</p>
+									</div>
+								</div>
+							</div>
 						</div>
 					))}
 				</div>
