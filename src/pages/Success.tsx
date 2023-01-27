d97c4a2c284f861c7476307ec54557e61f5fad43
LucasOe <lucas.oelker@gmx.de>
2023-01-27 19:41:54 +0100

    Add success page

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,20 +1,16 @@
-import { useRouteError } from "react-router-dom";
+import { SlCheck } from "react-icons/sl";
 import LinkButton from "../components/LinkButton";
 
-type ErrorResponse = {
-	status: number;
-	statusText: string;
-};
-
 export default function ErrorPage() {
-	const error: ErrorResponse = useRouteError() as ErrorResponse;
-
 	return (
 		<div className="flex min-h-screen justify-center bg-neutral-900 p-6">
 			<div className="h-full sm:pt-20 md:pt-40 lg:pt-60 xl:pt-80">
 				<div className="flex flex-col items-center text-center text-neutral-200">
-					<p className="font-mono text-xxl font-bold leading-none">{error.status}</p>
-					<p className="font-sans text-5xl font-medium text-neutral-400">{error.statusText}</p>
+					<SlCheck className="m-8 h-1/3 w-1/3 text-green-400" />
+					<p className="mb-2 font-sans text-2xl font-medium text-green-400">Form submitted successfully!</p>
+					<p className="font-sans font-medium text-neutral-400">
+						Thank you! The form has been submitted successfully. <br />I will reply to you soon!
+					</p>
 					<LinkButton link={"/"} text={"Go Home"} className="mt-10 w-32" />
 				</div>
 			</div>
