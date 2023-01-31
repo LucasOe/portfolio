4b856de4fcffe35deb1e923d15d8e54b4e853b09
LucasOe <github@lucasoe.com>
2023-01-31 21:46:15 +0100

    Rework Skills Section

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,19 +1,29 @@
-import Label from "./Label";
+import { IconContext } from "react-icons";
 
 type SkillsProps = {
-	text: string;
-	skills: string[];
+	children: React.ReactNode;
+	title: string;
+	icon: React.ReactNode;
 	className?: string;
 };
 
-export default function Skills({ text, skills, className }: SkillsProps) {
+export default function Skills({ children, title, icon, className }: SkillsProps) {
 	return (
-		<div className={`${className} w-full rounded-md bg-neutral-700 p-2`}>
-			<h3 className="m-0 mb-2 font-mono">{text}</h3>
+		<div className={`${className} w-full rounded-md bg-neutral-700 p-4`}>
+			<div className="mb-2 flex items-center justify-between gap-4">
+				<h3 className="m-0 font-mono">{title}</h3>
+				<IconContext.Provider value={{ className: "w-10 h-10" }}>{icon}</IconContext.Provider>
+			</div>
 			<div className="flex gap-2">
-				{skills.map((skill, index) => (
-					<Label key={index} text={skill} />
-				))}
+				<div
+					className="flex select-none flex-col items-center justify-between
+					font-mono text-sm font-semibold leading-normal text-neutral-500"
+				>
+					<span>&#60;h3&#62;</span>
+					<div className="m-auto my-2 h-full w-[2px] rounded-full bg-neutral-500" />
+					<span>&#60;/h3&#62;</span>
+				</div>
+				<p>{children}</p>
 			</div>
 		</div>
 	);
