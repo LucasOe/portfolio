bc584fb5355a454fa676aaa45de6c0a3b134d6c7
LucasOe <github@lucasoe.com>
2023-01-31 19:46:01 +0100

    Change sans-sarif font family

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
 	plugins: [
 		react(),
 		webfontDownload([
-			"https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap",
+			"https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap",
 			"https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap",
 			"https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&display=swap",
 		]),
