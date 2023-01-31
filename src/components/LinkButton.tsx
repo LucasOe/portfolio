a9a857c7c372ea08a1826d63958b846eb13220f9
LucasOe <lucas.oelker@gmx.de>
2023-01-31 20:41:55 +0100

    Add focus outlines

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -8,8 +8,12 @@ type LinkButtonProps = {
 
 export default function LinkButton({ link, text, className }: LinkButtonProps) {
 	return (
-		<Link to={link} className={`${className} inline-block`}>
-			<div className="rounded-full bg-neutral-200 p-2 opacity-80 hover:opacity-100">
+		<Link
+			to={link}
+			className={`${className} inline-block overflow-hidden rounded-lg
+			focus:outline focus:outline-2 focus:outline-green-400`}
+		>
+			<div className="bg-neutral-200 p-2 opacity-80 hover:opacity-100">
 				<p className="px-5 font-medium text-neutral-900">{text}</p>
 			</div>
 		</Link>
