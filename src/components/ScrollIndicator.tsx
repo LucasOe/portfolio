1fa3503263b0cf21cfbb32747c4ca780fd6432e1
LucasOe <github@lucasoe.com>
2023-02-10 00:21:53 +0100

    Animate ScrollIndicator

---
+++ src/components/ScrollIndicator.tsx
@@ -0,0 +1,22 @@
+import { motion, useScroll, useTransform } from "framer-motion";
+import { BsArrowDownShort } from "react-icons/bs";
+import { HashLink } from "react-router-hash-link";
+
+type ScrollIndicatorType = {
+	className: string;
+};
+
+export default function ScrollIndicator({ className }: ScrollIndicatorType) {
+	const { scrollY } = useScroll();
+	// Map values from [0, 500] to [1, 0]
+	const opacity = useTransform(scrollY, [0, window.innerHeight / 2], [1, 0]);
+	return (
+		<motion.div style={{ opacity: opacity }} className={className}>
+			<HashLink to="#about" smooth aria-label="Scroll Down" tabIndex={-1}>
+				<div className="m-2 animate-bounce rounded-full bg-white/50 hover:bg-white/75">
+					<BsArrowDownShort className="h-12 w-12 text-neutral-800" />
+				</div>
+			</HashLink>
+		</motion.div>
+	);
+}
