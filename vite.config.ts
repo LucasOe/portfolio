c4134c99e986ddb819b4362f35bfc3bf034e7292
LucasOe <github@lucasoe.com>
2023-07-22 06:11:11 +0200

    Update packages and replace Babel with SWC

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react";
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import webfontDownload from "vite-plugin-webfont-dl";
 
