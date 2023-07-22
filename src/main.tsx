d9f0c0f40d094ce502f0bf713e34ddb0e7b973bd
LucasOe <github@lucasoe.com>
2023-07-23 00:40:02 +0200

    Fix import

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./main.css";
 import ErrorPage from "./pages/ErrorPage";
 import Home from "./pages/Home";
-import Privacy from "./pages/privacy";
+import Privacy from "./pages/Privacy";
 import Success from "./pages/Success";
 
 const router = createBrowserRouter([
