eb9fb727194d4fc4422e634c464d81e7232d9c96
LucasOe <github@lucasoe.com>
2023-07-23 03:07:27 +0200

    Add dedent

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -1,4 +1,6 @@
+import dedent from "dedent";
 import { SlCheck } from "react-icons/sl";
+import ReactMarkdown from "react-markdown";
 import LinkButton from "../components/LinkButton";
 
 export default function ErrorPage() {
@@ -7,10 +9,15 @@ export default function ErrorPage() {
 			<div className="h-full sm:pt-20 md:pt-40 lg:pt-60 xl:pt-80">
 				<div className="flex flex-col items-center text-center text-neutral-200">
 					<SlCheck className="m-8 h-1/3 w-1/3 text-green-400" />
-					<p className="mb-2 font-sans text-2xl font-medium text-green-400">Form submitted successfully!</p>
-					<p className="font-sans font-medium text-neutral-400">
-						Thank you! The form has been submitted successfully. <br />I will reply to you soon!
-					</p>
+					<ReactMarkdown className="mb-2 font-sans text-2xl font-medium text-green-400">
+						Form submitted successfully!
+					</ReactMarkdown>
+					<ReactMarkdown className="font-sans font-medium text-neutral-400">
+						{dedent`
+							Thank you! The form has been submitted successfully.  
+							I will reply to you soon!
+						`}
+					</ReactMarkdown>
 					<LinkButton link={"/"} text={"Go Home"} className="mt-10 w-32" />
 				</div>
 			</div>
