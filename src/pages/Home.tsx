3ffc24e66f1afa97b6f58a2af6e7227736f29219
LucasOe <lucas.oelker@gmx.de>
2023-07-23 03:07:27 +0200

    Add dedent

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import dedent from "dedent";
 import { BiBrush } from "react-icons/bi";
 import { BsPeople } from "react-icons/bs";
 import { FaReact } from "react-icons/fa";
@@ -27,15 +28,16 @@ export default function Home() {
 						</div>
 						<Header text="About" id="about" />
 						<ReactMarkdown>
-							My name is Lucas Oelker and I’m a software developer currently studying Visual Computing and
-							Design in Lippstadt, Germany. Programming, rendering, or web design — if it requires a
-							computer and some creative thinking I’m interested!
-						</ReactMarkdown>
-						<ReactMarkdown>
-							I have a deep passion for programming and I like solving problems. I first started learning
-							Java and the basics of web development in high school. Today I’m studying Visual Computing
-							and Design, which allows me to think about the integrated approach to the conception and
-							development of technical systems as well as their conceptual and design perspective.
+							{dedent`
+								My name is Lucas Oelker and I’m a software developer currently studying Visual Computing and
+								Design in Lippstadt, Germany. Programming, rendering, or web design — if it requires a
+								computer and some creative thinking I’m interested!
+
+								I have a deep passion for programming and I like solving problems. I first started learning
+								Java and the basics of web development in high school. Today I’m studying Visual Computing
+								and Design, which allows me to think about the integrated approach to the conception and
+								development of technical systems as well as their conceptual and design perspective.
+							`}
 						</ReactMarkdown>
 						<Header text="Project Timeline" id="projects" />
 						<Timeline
@@ -46,9 +48,11 @@ export default function Home() {
 									stack: ["Java", "JavaFX", "MySQL"],
 									time: 1619820000,
 									arrowPosition: 0,
-									description: `*Das Rezeptbuch* is a digital cookbook. The application provides an interface built in
-								JavaFX that lets you view recipes stored in a database. This project was created in a
-								team effort for the university module *Computer Science*.`,
+									description: dedent`
+										*Das Rezeptbuch* is a digital cookbook. The application provides an interface built in
+										JavaFX that lets you view recipes stored in a database. This project was created in a
+										team effort for the university module *Computer Science*.
+									`,
 								},
 								{
 									title: "Releaf",
@@ -56,9 +60,11 @@ export default function Home() {
 									stack: ["HTML", "SASS", "JavaScript", "PHP", "MySQL"],
 									time: 1635721200,
 									arrowPosition: 50,
-									description: `*Releaf* is a website and brand identity for an online shop that sells tea for relaxation
-								and health. The project was created in a team effort for the university module *Web- and
-								Corporate Design* on the topic of health.`,
+									description: dedent`
+										*Releaf* is a website and brand identity for an online shop that sells tea for relaxation
+										and health. The project was created in a team effort for the university module *Web- and
+										Corporate Design* on the topic of health.
+									`,
 								},
 								{
 									title: "Marble Physics Simulation",
@@ -66,9 +72,11 @@ export default function Home() {
 									stack: ["Java"],
 									time: 1646089200,
 									arrowPosition: 50,
-									description: `A 2D Physics simulation, created for the university module *Visual Computing*.  
-								The model simulates a 2D marble run with physically accurate calculations for gravity,
-								friction, collisions, and a pendulum.`,
+									description: dedent`
+										A 2D Physics simulation, created for the university module *Visual Computing*.  
+										The model simulates a 2D marble run with physically accurate calculations for gravity,
+										friction, collisions, and a pendulum.
+									`,
 								},
 								{
 									title: "Portfolio",
@@ -76,9 +84,11 @@ export default function Home() {
 									stack: ["Vite", "React", "Tailwind CSS", "TypeScript"],
 									time: 1659304800,
 									arrowPosition: 50,
-									description: `You’re looking at it right now. After building multiple websites using HTML, CSS, and
-								PHP, this project was my first deep dive into the world of modern web development,
-								using a tech stack including Vite, Typescript, React, and Tailwind CSS.`,
+									description: dedent`
+										You’re looking at it right now. After building multiple websites using HTML, CSS, and
+										PHP, this project was my first deep dive into the world of modern web development,
+										using a tech stack including Vite, Typescript, React, and Tailwind CSS.
+									`,
 								},
 								{
 									title: "Tabletop Simulator API",
@@ -86,28 +96,38 @@ export default function Home() {
 									stack: ["Rust"],
 									time: 1673910000,
 									arrowPosition: 100,
-									description: `A Rust API for interacting with the game Tabletop Simulator using TCP messages.  
-								I created this project as an opportunity to learn more about Rust and API design.`,
+									description: dedent`
+										A Rust API for interacting with the game Tabletop Simulator using TCP messages.  
+										I created this project as an opportunity to learn more about Rust and API design.
+									`,
 								},
 							]}
 						/>
 						<Header text="Skills" id="skills" />
 						<div className="grid gap-3 sm:grid-cols-2">
 							<Skills title="Software Development" icon={<FiMonitor />}>
-								I have five years of academic education in **Java** and I’m currently learning **Rust**.
-								I also have experience in game development using **Unity** and **C#**.
+								{dedent`
+									I have five years of academic education in **Java** and I’m currently learning **Rust**.
+									I also have experience in game development using **Unity** and **C#**.
+								`}
 							</Skills>
 							<Skills title="Frontend Development" icon={<FaReact />}>
-								I am proficient in web development using **HTML**, **CSS**, and **PHP**. Currently, I’m
-								learning to use modern frameworks like **React** and **Tailwind CSS**.
+								{dedent`								
+									I am proficient in web development using **HTML**, **CSS**, and **PHP**. Currently, I’m
+									learning to use modern frameworks like **React** and **Tailwind CSS**.
+								`}
 							</Skills>
 							<Skills title="Design" icon={<BiBrush />}>
-								The university degree I’m pursuing doesn’t just cover Computer Science but also Design
-								and User Experience! I have worked with **Adobe XD** and **Figma** in the past.
+								{dedent`								
+									The university degree I’m pursuing doesn’t just cover Computer Science but also Design
+									and User Experience! I have worked with **Adobe XD** and **Figma** in the past.
+								`}
 							</Skills>
 							<Skills title="Cooperation" icon={<BsPeople />}>
-								I have worked in groups on many projects for school and university. In my free time I
-								like to work on Open Source projects on Github.
+								{dedent`
+									I have worked in groups on many projects for school and university. In my free time I
+									like to work on Open Source projects on Github.
+								`}
 							</Skills>
 						</div>
 						<Header text="Contact" id="contact" />
