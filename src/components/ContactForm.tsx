67f5e7c6a43b6f29951d27babe5ed647030c7504
LucasOe <github@lucasoe.com>
2023-07-24 22:33:21 +0200

    Add form class

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -11,37 +11,37 @@ export default function ContactForm() {
 				<input type="hidden" name="redirect" value="https://lucasoe.com/success" />
 				<div className="flex flex-col gap-3 sm:flex-row">
 					<label className="w-full">
-						<span className="pl-1 font-bold">Name</span>
+						<span className="pl-1 font-bold text-neutral-100">Name</span>
 						<input
 							type="text"
 							name="name"
 							id="name"
 							placeholder="Enter Your Name"
 							required
-							className="focus w-full rounded-md border-none bg-neutral-700 placeholder:text-neutral-500"
+							className="form"
 						/>
 					</label>
 					<label className="w-full">
-						<span className="pl-1 font-bold">E-Mail</span>
+						<span className="pl-1 font-bold text-neutral-100">E-Mail</span>
 						<input
 							type="email"
 							name="email"
 							id="email"
 							placeholder="Enter Your E-Mail"
 							required
-							className="focus w-full rounded-md border-none bg-neutral-700 placeholder:text-neutral-500"
+							className="form"
 						/>
 					</label>
 				</div>
 				<label className="w-full">
-					<span className="pl-1 font-bold">Message</span>
+					<span className="pl-1 font-bold text-neutral-100">Message</span>
 					<textarea
 						name="message"
 						id="message"
 						placeholder="Enter Your Message"
 						rows={8}
 						required
-						className="focus w-full resize-none rounded-md border-none bg-neutral-700 placeholder:text-neutral-500"
+						className="form resize-none"
 					/>
 				</label>
 				<button type="submit" className="focus button self-end">
