8a2857705ee76c39b93cbd781056e70a7f71d34b
LucasOe <github@lucasoe.com>
2023-07-24 23:19:23 +0200

    Fix Navbar and Footer margins

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { HashLink } from "react-router-hash-link";
 
 export default function Footer() {
 	return (
-		<div className="absolute bottom-0 m-6 flex w-full justify-center">
+		<div className="absolute bottom-0 flex w-full justify-center">
 			<div className="prose prose-invert">
 				<HashLink to="/privacy#" className="focus rounded-sm no-underline outline-none">
 					Privacy Policy
