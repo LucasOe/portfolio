70fc638d1962ccd4d12604e6ac4aa5d4c76af036
LucasOe <github@lucasoe.com>
2023-08-06 17:30:24 +0200

    Remove Tilt effect from Hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,17 +1,8 @@
-import Tilt from "react-parallax-tilt";
 import { TypeAnimation } from "react-type-animation";
 
 export default function Hero({ className }: React.HTMLAttributes<HTMLDivElement>) {
 	return (
-		<Tilt
-			tiltReverse={true}
-			tiltMaxAngleX={12}
-			tiltMaxAngleY={6}
-			transitionSpeed={1500}
-			trackOnWindow={true}
-			gyroscope={true}
-			className={`select-none text-[7vw] leading-normal max-md:!transform-none md:text-[4rem] lg:text-[5rem] ${className}`}
-		>
+		<div className={`select-none text-[7vw] leading-normal md:text-[4rem] lg:text-[5rem] ${className}`}>
 			<p className="m-0 font-bold">Hey, I’m Lucas Oelker.</p>
 			<p className="m-0 font-normal">
 				{"I’m a "}
@@ -36,6 +27,6 @@ export default function Hero({ className }: React.HTMLAttributes<HTMLDivElement>
 					className="text-green-400 after:text-white"
 				/>
 			</p>
-		</Tilt>
+		</div>
 	);
 }
