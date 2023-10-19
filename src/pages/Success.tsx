3fe6ea8a79ed4be6cc3c224371deeee24c5c57e1
LucasOe <github@lucasoe.com>
2023-10-19 23:23:41 +0200

    Fix component name

--- src/pages/Success.tsx
+++ src/pages/Success.tsx
@@ -5,7 +5,7 @@ import { SlCheck } from "react-icons/sl";
 
 import LinkButton from "@/components/LinkButton";
 
-export default function ErrorPage() {
+export default function SuccessPage() {
 	return (
 		<div className="not-prose mb-24 text-center">
 			<SlCheck className="m-8 mx-auto h-32 w-32 text-green-400" />
