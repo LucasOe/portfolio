5322c3ab9e337f5796b2681e8d8a3b69629b26db
LucasOe <lucas.oelker@gmx.de>
2023-10-19 23:07:46 +0200

    Add prop type for every component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,9 @@
 import { HashLink } from "react-router-hash-link";
 import { twMerge } from "tailwind-merge";
 
-export default function Footer({ className }: React.ComponentProps<"div">) {
+export type FooterProps = React.ComponentProps<"div">;
+
+export default function Footer({ className }: FooterProps) {
 	return (
 		<div className={twMerge("not-prose absolute bottom-0 my-6 flex w-full justify-center", className)}>
 			<HashLink to="/privacy#" className="focus rounded-sm px-1 leading-snug no-underline">
