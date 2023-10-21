ca4c61ebf7ef0f3a08f32befe1ff10356b0f1e7d
LucasOe <lucas.oelker@gmx.de>
2023-10-21 17:01:07 +0200

    Use ElementRef

--- src/components/ScrollIndicator.tsx
+++ src/components/ScrollIndicator.tsx
@@ -7,8 +7,9 @@ export type ScrollIndicatorProps = React.ComponentProps<typeof motion.div>;
 
 export default function ScrollIndicator({ className, ...rest }: ScrollIndicatorProps) {
 	const { scrollY } = useScroll();
-	// Map values from [0, 500] to [1, 0]
+	// Map values from [0, h/2] to [1, 0] for opacity
 	const opacity = useTransform(scrollY, [0, window.innerHeight / 2], [1, 0]);
+
 	return (
 		<motion.div style={{ opacity: opacity }} className={className} {...rest}>
 			<HashLink to="#about" smooth aria-label="Scroll Down" tabIndex={-1}>
