b9a7a95ed4e4613ca3c86d473b5618596d115b5d
LucasOe <github@lucasoe.com>
2023-11-04 20:38:30 +0100

    Remove button layer

--- src/components/LinkButton.tsx
+++ src/components/LinkButton.tsx
@@ -8,7 +8,14 @@ export type LinkButtonProps = React.ComponentProps<typeof HashLink> & {
 
 export default function LinkButton({ text, to, className, ...rest }: LinkButtonProps) {
 	return (
-		<HashLink to={to} className={twMerge("not-prose focus button", className)} {...rest}>
+		<HashLink
+			to={to}
+			className={twMerge(
+				"not-prose focus focus-neutral-300 inline-block rounded-lg bg-neutral-300 p-1 hover:bg-neutral-100 text-neutral-900 font-medium px-5 no-underline outline-none",
+				className
+			)}
+			{...rest}
+		>
 			{text}
 		</HashLink>
 	);
