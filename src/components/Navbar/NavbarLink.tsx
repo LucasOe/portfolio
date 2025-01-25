e33f865712418bbb5402c8ec64b779802d6cb162
LucasOe <github@lucasoe.com>
2025-01-25 17:23:02 +0100

    Add text-gradient utility

--- src/components/Navbar/NavbarLink.tsx
+++ src/components/Navbar/NavbarLink.tsx
@@ -16,9 +16,7 @@ export default function NavbarLink({ text, to, className, ...rest }: NavbarLinkP
 			className={twMerge("focus p-2 no-underline outline-hidden", className)}
 			{...rest}
 		>
-			<span className="hover:bg-accent-violet hover:from-accent-pink font-mono text-lg font-semibold hover:bg-gradient-to-bl hover:bg-cover hover:bg-clip-text hover:text-transparent">
-				{`//${text}`}
-			</span>
+			<span className="hover:text-gradient font-mono text-lg font-semibold">{`//${text}`}</span>
 		</HashLink>
 	);
 }
