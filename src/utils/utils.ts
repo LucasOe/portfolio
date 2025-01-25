ae146f79fe8276bec2a8cac90be959f04a003667
LucasOe <github@lucasoe.com>
2025-01-25 17:40:49 +0100

    Fix formatting

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,9 @@
 import type { ComponentRef, RefObject } from "react";
 
-interface Pos { x: number; y: number }
+interface Pos {
+	x: number;
+	y: number;
+}
 
 // Returns the distance of the cursor relative to the center of the ref object
 export function getRelativeMousePos(mosePos: Pos, ref: RefObject<ComponentRef<"div"> | null>): Pos {
