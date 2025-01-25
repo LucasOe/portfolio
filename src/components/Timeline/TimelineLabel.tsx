6f4778f252d01e6dac90cb4d7d64d65247e30db0
LucasOe <github@lucasoe.com>
2025-01-25 22:16:33 +0100

    Update border width in TimelineLabel component

--- src/components/Timeline/TimelineLabel.tsx
+++ src/components/Timeline/TimelineLabel.tsx
@@ -6,7 +6,7 @@ export interface TimelineLabelProps extends React.ComponentProps<"div"> {
 
 export default function Label({ text, className, ...rest }: TimelineLabelProps) {
 	return (
-		<div className={twMerge("rounded-lg border-[3px] border-neutral-300", className)} {...rest}>
+		<div className={twMerge("rounded-lg border-3 border-neutral-300", className)} {...rest}>
 			<p className="px-3 py-1 font-mono text-base font-bold">{text}</p>
 		</div>
 	);
