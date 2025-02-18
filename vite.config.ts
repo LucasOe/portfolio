56569771c14adbabf6a790972214deadba23af7a
LucasOe <github@lucasoe.com>
2025-02-18 10:56:22 +0100

    Add react-router-serve

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,6 @@ import { webfontDownload } from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	build: {
-		sourcemap: true,
-	},
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
