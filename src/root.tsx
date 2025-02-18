64d0aede9b3ec1124083a10aec9e71ecc8b593fb
LucasOe <github@lucasoe.com>
2025-02-18 10:21:28 +0100

    Update ErrorBoundary and .gitignore

--- src/root.tsx
+++ src/root.tsx
@@ -1,4 +1,8 @@
-import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "react-router";
+import { Links, Meta, Outlet, Scripts, ScrollRestoration, isRouteErrorResponse } from "react-router";
+
+import type { Route } from "./+types/root";
+
+import LinkButton from "@/components/LinkButton";
 
 import "@/main.css";
 
@@ -32,4 +36,27 @@ export function Layout({ children }: { children: React.ReactNode }) {
 
 export default function Root() {
 	return <Outlet />;
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+	if (isRouteErrorResponse(error)) {
+		return (
+			<main className="mb-24 text-center">
+				<p className="font-mono text-[12rem] leading-none font-bold">{error.status}</p>
+				<p className="text-5xl font-medium text-neutral-500">{error.statusText}</p>
+				<LinkButton text={"Go Home"} to={"/"} className="m-10" />
+			</main>
+		);
+	} else if (error instanceof Error) {
+		return (
+			<main className="mb-24 text-center">
+				<h1>Error</h1>
+				<p>{error.message}</p>
+				<p>The stack trace is:</p>
+				<pre className="font-mono">{error.stack}</pre>
+			</main>
+		);
+	} else {
+		return <h1>Unknown Error</h1>;
+	}
 }
