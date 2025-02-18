e7465409c0e672a7fc67e98d5e49211eda74be78
LucasOe <github@lucasoe.com>
2025-02-18 17:14:16 +0100

    Add @vercel/speed-insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -36,5 +37,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
 		<RouterProvider router={router} />
+		<SpeedInsights />
 	</React.StrictMode>
 );
