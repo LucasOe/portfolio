ce82adc14d04efca82319667856be1a47d4a443f
LucasOe <github@lucasoe.com>
2025-02-18 11:59:19 +0100

    Remove vite-plugin-webfont-dl

--- app/root.tsx
+++ app/root.tsx
@@ -4,7 +4,29 @@ import type { Route } from "./+types/root";
 
 import LinkButton from "./components/LinkButton";
 
-import "@/main.css";
+import stylesheet from "@/main.css?url";
+
+export const links: Route.LinksFunction = () => [
+	{ rel: "preconnect", href: "https://fonts.googleapis.com" },
+	{
+		rel: "preconnect",
+		href: "https://fonts.gstatic.com",
+		crossOrigin: "anonymous",
+	},
+	{
+		rel: "stylesheet",
+		href: "https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap",
+	},
+	{
+		rel: "stylesheet",
+		href: "https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap",
+	},
+	{
+		rel: "stylesheet",
+		href: "https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&display=swap",
+	},
+	{ rel: "stylesheet", href: stylesheet },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
 	return (
