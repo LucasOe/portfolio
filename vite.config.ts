df6e570588ddcba4f9014818c1e63fae0e44df11
LucasOe <github@lucasoe.com>
2025-02-18 11:59:19 +0100

    Remove vite-plugin-webfont-dl

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { webfontDownload } from "vite-plugin-webfont-dl";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,14 +12,5 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./app"),
 		},
 	},
-	plugins: [
-		reactRouter(),
-		tsconfigPaths(),
-		tailwindcss(),
-		webfontDownload([
-			"https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap",
-			"https://fonts.googleapis.com/css2?family=Libre+Baskerville:wght@400;700&display=swap",
-			"https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;700&display=swap",
-		]),
-	],
+	plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
 });
