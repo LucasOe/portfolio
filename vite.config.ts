066184ea2bd605c0f982974382da766c12126842
LucasOe <github@lucasoe.com>
2025-02-18 12:00:53 +0100

    Remove unnecessary path alias configuration from Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-import path from "node:path";
-
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -7,10 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	resolve: {
-		alias: {
-			"@": path.resolve(__dirname, "./app"),
-		},
-	},
 	plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
 });
