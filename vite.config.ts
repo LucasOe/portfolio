5398b904e15c72852c48354096d48c20cef428d9
LucasOe <github@lucasoe.com>
2025-02-20 18:12:16 +0100

    Disable minified newlines

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+	esbuild: {
+		supported: {
+			// Disable minified newlines
+			// https://esbuild.github.io/faq/#minified-newlines
+			"template-literal": false,
+		},
+	},
 	plugins: [reactRouter(), tsconfigPaths(), tailwindcss()],
 });
