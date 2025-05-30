72d85166cb45077b21178b4d177895d39deee655
LucasOe <github@lucasoe.com>
2025-05-30 05:31:17 +0200

    Add workaround for scroll reset in Firefox

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,4 +1,4 @@
-import { href, Link } from "react-router";
+import { href } from "react-router";
 import { twMerge } from "tailwind-merge";
 
 export type FooterProps = React.ComponentProps<"div">;
@@ -6,9 +6,10 @@ export type FooterProps = React.ComponentProps<"div">;
 export default function Footer({ className, ...rest }: FooterProps) {
 	return (
 		<div className={twMerge("flex w-full justify-center p-6", className)} {...rest}>
-			<Link to={href("/privacy")} className="focus px-2 py-0.5 text-lg text-neutral-500 no-underline">
+			{/* https://github.com/remix-run/react-router/issues/13084 */}
+			<a href={href("/privacy")} className="focus px-2 py-0.5 text-lg text-neutral-500 no-underline">
 				Privacy Policy
-			</Link>
+			</a>
 		</div>
 	);
 }
