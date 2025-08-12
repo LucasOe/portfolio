87988faea6c79abf690ca5b76535fb6e2561e56f
LucasOe <github@lucasoe.com>
2025-08-12 16:23:53 +0200

    Fix scroll-margin regression

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,7 +6,10 @@ export interface HeaderProps extends React.ComponentProps<"div"> {
 
 export default function Header({ text, className, ...rest }: HeaderProps) {
 	return (
-		<h2 className={twMerge("mt-24 mb-8 font-mono text-3xl leading-10 font-bold", className)} {...rest}>
+		<h2
+			className={twMerge("mt-24 mb-8 scroll-m-[12vh] font-mono text-3xl leading-10 font-bold", className)}
+			{...rest}
+		>
 			{`# ${text}`}
 		</h2>
 	);
