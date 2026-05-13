b5078dff26d25c2131697c81e30f78858545f0c4
LucasOe <github@lucasoe.com>
2026-05-13 18:21:06 +0200

    Update vite.config.ts to Vite 8

--- vite.config.ts
+++ vite.config.ts
@@ -3,29 +3,16 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
-import viteTsConfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	esbuild: {
-		supported: {
-			// Disable minified newlines
-			// https://esbuild.github.io/faq/#minified-newlines
-			"template-literal": false,
-		},
+	resolve: {
+		tsconfigPaths: true,
 	},
 	plugins: [
-		// this is the plugin that enables path aliases
-		viteTsConfigPaths({
-			projects: ["./tsconfig.json"],
-		}),
 		tailwindcss(),
 		tanstackStart(),
 		nitro(),
-		viteReact({
-			babel: {
-				plugins: ["babel-plugin-react-compiler"],
-			},
-		}),
+		viteReact(),
 	],
 });
